22014e46297ec06457b371681549c7b73f08fb7d
subho808 <subhojitsaha808@gmail.com>
2025-07-15 11:32:40 +0530

    favicon syntax change

--- src/app/page.js
+++ src/app/page.js
@@ -100,7 +100,7 @@ export default function Home() {
   <link rel="canonical" href="https://msrdevxpert.com/" />
 
   {/* Favicon */}
-  <link rel="icon" href="/favicon.ico" sizes="any" />
+  <link rel="icon" href="/favicon.ico"/>
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
